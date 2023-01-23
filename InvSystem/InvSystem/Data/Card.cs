0f2a8b4c69e0d9ec88e0b57358fd086d36ed0502
Niklasito <niklasandersson.gbg@gmail.com>
2023-01-23 14:44:12 +0100

    Sorted out the carousel, added filter

--- InvSystem/InvSystem/Data/Card.cs
+++ InvSystem/InvSystem/Data/Card.cs
@@ -7,6 +7,8 @@ namespace InvSystem.Data
         public string Img_url { get; set; }
         public string Title { get; set; }
 
+        public string Category { get; set; }
+
         public double Price { get; set; }
 
         public double JmfrPris { get; set; }
