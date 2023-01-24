d78ca3ac5b242f5445935272b62cf2b6b6b5eca3
Niklasito <niklasandersson.gbg@gmail.com>
2023-01-25 00:28:36 +0100

    Edited a lot of css to different components.

--- Application.UnitTests/UnitTest1.cs
+++ Application.UnitTests/UnitTest1.cs
@@ -1,10 +1,25 @@
 namespace Application.UnitTests;
 
-public class UnitTest1
+public class SearchTests
 {
     [Fact]
-    public void Test1()
+    public void When_Entering_Search_Input_Return_List_Of_Groceries()
     {
+        // Arrange
+        var searchInput = "Banan";
+        var expected = new List<Grocery>
+        {
+            new Grocery { Name = "Banan", Price = 1.99m },
+            new Grocery { Name = "Banan Ekologisk", Price = 2.99m }
+        };
 
+       
+
+        // Act
+        var actual = searchService.Search(searchInput);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    
     }
 }
