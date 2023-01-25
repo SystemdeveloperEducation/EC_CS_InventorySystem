4b8ebd8e9a92cda6ff721bd39448d3f60f562615
andresparrab <andres.parrab@gmail.com>
2023-01-25 10:46:03 +0100

    till niklasito, dog luck

--- Application.UnitTests/UnitTest1.cs
+++ Application.UnitTests/UnitTest1.cs
@@ -6,20 +6,42 @@ public class SearchTests
     public void When_Entering_Search_Input_Return_List_Of_Groceries()
     {
         // Arrange
-        var searchInput = "Banan";
+        var category = "Banan";
+        var groceriesClient = new HttpClient();
+        var cards = new List<Card>()
+        {
+            new Card() // kött
+            new Card() //äpple
+            new Card() // Bannan
+            new Card() // mango
+            new Card() // mjölk
+        };
+
+        var response = groceriesClient.getAsync(any.string).ReturnsForAnyArgs(any.string);
+        var responceContent = response.Content.ReadAsStringAsync().ReturnsForAnyArgs(any.string);
+        var allacard = JsonConvert.DeserializeObject<List<Card>>(responceContent).ReturnsForAnyArgs(cards);
+
+
+
+
+
+
         var expected = new List<Grocery>
         {
             new Grocery { Name = "Banan", Price = 1.99m },
             new Grocery { Name = "Banan Ekologisk", Price = 2.99m }
+            new
+
+
         };
 
-       
+
 
         // Act
-        var actual = searchService.Search(searchInput);
+        var actual = fetchdata.GetApiInfo(category);
 
         // Assert
         Assert.Equal(expected, actual);
-    
+
     }
 }
